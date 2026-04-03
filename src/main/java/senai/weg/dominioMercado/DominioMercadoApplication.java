c12105842683679e2ff90fee84192276b66e30b3
andrMiotto <andre_lm_pereira@estudante.sesisenai.org.br>
2026-04-02 21:06:54 -0300

    feat:comeca

---
+++ src/main/java/senai/weg/dominioMercado/DominioMercadoApplication.java
@@ -0,0 +1,13 @@
+package senai.weg.dominioMercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DominioMercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DominioMercadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/senai/weg/dominioMercado/DominioMercadoApplicationTests.java
@@ -0,0 +1,13 @@
+package senai.weg.dominioMercado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DominioMercadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
